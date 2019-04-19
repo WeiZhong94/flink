4991a173e10a3c40e3ed7bfd77b4dd434561e36e
Dian Fu <fudian.fd@alibaba-inc.com>
2019-04-19 11:59:48 +0800

    refactor

--- flink-python/flink-python-table/src/main/python/pyflink/java_gateway.py
+++ flink-python/flink-python-table/src/main/python/pyflink/java_gateway.py
@@ -24,10 +24,12 @@ import time
 from subprocess import Popen, PIPE
 from threading import RLock
 
-from py4j.java_gateway import JavaGateway, GatewayClient
+from py4j.java_gateway import JavaGateway, GatewayParameters
+from pyflink.find_flink_home import _find_flink_home
 
 _gateway = None
 _lock = RLock()
+
 
 def get_gateway():
     # type: () -> JavaGateway
@@ -38,59 +40,55 @@ def get_gateway():
             # if Java Gateway is already running
             if 'PYFLINK_GATEWAY_PORT' in os.environ:
                 gateway_port = int(os.environ['PYFLINK_GATEWAY_PORT'])
-                _gateway = JavaGateway(GatewayClient(port=gateway_port), auto_convert=True)
+                _gateway = JavaGateway(GatewayParameters(port=gateway_port, auto_convert=True))
             else:
-                _gateway = launch_java_gateway()
+                _gateway = launch_gateway()
     return _gateway
 
 
-def launch_java_gateway():
+def launch_gateway():
     # type: () -> JavaGateway
+    """
+    launch jvm gateway
+    """
+
+    FLINK_HOME = _find_flink_home()
+    # TODO windows support
+    script = "./bin/pyflink2.sh"
+    gateway_server = ClassName.PYTHON_SHELL_GATEWAY_SERVER
+    command = [os.path.join(FLINK_HOME, script, '-c', gateway_server)]
+
+    # Create a temporary directory where the gateway server should write the connection information.
     conn_info_dir = tempfile.mkdtemp()
     try:
         fd, conn_info_file = tempfile.mkstemp(dir=conn_info_dir)
         os.close(fd)
         os.unlink(conn_info_file)
 
-        launch_java_process(conn_info_file)
+        env = dict(os.environ)
+        env["_PYFLINK_CONN_INFO_PATH"] = conn_info_file
+
+        def preexec_func():
+            # ignore ctrl-c / SIGINT
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+        # Launch the Java gateway.
+        # We open a pipe to stdin so that the Java gateway can die when the pipe is broken
+        p = Popen(command, stdin=PIPE, preexec_fn=preexec_func, env=env)
+
+        while not p.poll() and not os.path.isfile(conn_info_file):
+            time.sleep(0.1)
+
+        if not os.path.isfile(conn_info_file):
+            raise Exception("Java gateway process exited before sending its port number")
 
         with open(conn_info_file, "rb") as info:
-            java_port = struct.unpack("!I", info.read(4))[0]
+            gateway_port = struct.unpack("!I", info.read(4))[0]
     finally:
         shutil.rmtree(conn_info_dir)
 
-    # Connect to the java gateway
-    gateway = JavaGateway(GatewayClient(port=java_port), auto_convert=True)
-
-    return gateway
-
-
-def launch_java_process(conn_info_file):
-    # type: (str) -> Popen
-    flink_home = None
-    if 'FLINK_ROOT_DIR' in os.environ:
-        flink_home = os.environ['FLINK_ROOT_DIR']
-    elif 'FLINK_HOME' in os.environ:
-        flink_home = os.environ['FLINK_HOME']
-    if flink_home is None:
-        raise Exception('FLINK_ROOT_DIR or FLINK_HOME is not set')
-    bin_dir = flink_home + '/bin'
-
-    shell_gateway = ClassName.PYTHON_SHELL_GATEWAY_SERVER
-    command = [bin_dir+'/pyflink2.sh', '-c', shell_gateway, conn_info_file]
-
-    def preexec_func():
-        # ignore SIGINT
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-    p = Popen(command, stdin=PIPE, preexec_fn=preexec_func, env=dict(os.environ))
-
-    while not p.poll() and not os.path.isfile(conn_info_file):
-        time.sleep(0.1)
-
-    if not os.path.isfile(conn_info_file):
-        raise Exception("Java gateway process exited before sending its port number")
-    return p
+    # Connect to the gateway
+    return JavaGateway(GatewayParameters(port=gateway_port, auto_convert=True))
 
 
 class ClassName(object):
@@ -106,8 +104,7 @@ class ClassName(object):
     CSV_TABLE_SOURCE = "org.apache.flink.table.sources.CsvTableSource"
     CSV_TABLE_SINK = "org.apache.flink.table.sinks.CsvTableSink"
     WRITE_MODE = "org.apache.flink.core.fs.FileSystem.WriteMode"
-    TABLE_ENVIRONMENT = "org.apache.flink.table.api.TableEnvironment"
     EXECUTION_ENVIRONMENT = "org.apache.flink.api.java.ExecutionEnvironment"
+    TABLE_ENVIRONMENT = "org.apache.flink.table.api.TableEnvironment"
     STREAM_EXECUTION_ENVIRONMENT = "org.apache.flink.streaming.api.environment.StreamExecutionEnvironment"
     PYTHON_SHELL_GATEWAY_SERVER = "org.apache.flink.api.python.PythonShellGatewayServer"
-
