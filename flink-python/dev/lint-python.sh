742d7a48f8cc8aca99d2914254def31a84f7793d
Wei Zhong <weizhong0618@gmail.com>
2019-07-17 16:46:11 +0800

    tmp commit

--- flink-python/dev/lint-python.sh
+++ flink-python/dev/lint-python.sh
@@ -213,7 +213,7 @@ function install_tox() {
         fi
     fi
 
-    $CONDA_PATH install -p $CONDA_HOME -c conda-forge virtualenv -y -q 2>&1 >/dev/null
+    $CONDA_PATH install -p $CONDA_HOME virtualenv -y -q 2>&1 >/dev/null
     $CONDA_PATH install -p $CONDA_HOME -c conda-forge tox -y -q 2>&1 >/dev/null
     if [ $? -ne 0 ]; then
         echo "conda install tox failed \
